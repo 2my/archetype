7eb8c1ebca42896da3acc2156459f0ab4478e6ec
AntaTom <tsk@antares.no>
2012-06-15 01:40:55 -0700

    Added module for application (Swing or Command-line)

---
+++ archetype-app/src/main/java/no/antares/kickstart/KickStart.java
@@ -0,0 +1,14 @@
+package no.antares.kickstart;
+
+
+/**
+ * @author Tommy Skodje
+ */
+public class KickStart {
+
+    /**	 */
+    public static void main( String[] args ) {
+    	System.out.println( "Hello World!" );
+    }
+
+}
