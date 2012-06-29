c7cd9563629afbcd027017e119624353fe9e8ac3
AntaTom <tsk@antares.no>
2012-06-29 00:23:16 -0700

    Added comments

--- archetype-app/src/main/java/no/antares/kickstart/ProcessControl.java
+++ archetype-app/src/main/java/no/antares/kickstart/ProcessControl.java
@@ -3,6 +3,10 @@ package no.antares.kickstart;
 
 /**
  * mvn exec:java -Dexec.mainClass="no.antares.kickstart.ProcessControl"
+http://stackoverflow.com/questions/830641/is-it-possible-to-have-interprocess-communication-in-java
+http://publib.boulder.ibm.com/infocenter/iseries/v7r1m0/index.jsp?topic=%2Frzaha%2Fsockets.htm
+
+http://www.coderanch.com/t/328888/java/java/Killing-process-spawned-Runtime-exec
  * @author Tommy Skodje
  */
 public class ProcessControl {
